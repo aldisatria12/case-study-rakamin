f2e52088418a23e9da6d3d99cf65741a9560f995
aldisatria12 <aldisatria253@gmail.com>
2023-11-05 18:12:52 +0700

    init case-study2

---
+++ case-study2/case-study2UITests/case_study2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  case_study2UITestsLaunchTests.swift
+//  case-study2UITests
+//
+//  Created by Aldi Mahotma on 05/11/23.
+//
+
+import XCTest
+
+final class case_study2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
